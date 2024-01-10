6720ed6944db3aa5a3cf6a9ff4364f751f3176a9
roctbb <roctbb@gmail.com>
2024-01-10 12:49:42 +0300

    Improve game session interaction by identifying actual user
    Changed the condition to check if the user and winner of the games are the same in the sessions' HTML template. It also changed the winner ID to reference user IDs instead of team IDs in the Session model. Additionally, enabling game sessions to be restarted from the list without opening a new tab. A relationship field for a created session was also removed from the User model, and equivalent creator and winner relationships are set in Session model. This adjustment will enhance user experience by providing straightforward session management and synchronization.

--- models/models.py
+++ models/models.py
@@ -27,7 +27,6 @@ class User(db.Model):
     login = db.Column(db.String(128), nullable=False)
     password = db.Column(db.String(1024), nullable=False)
     teams = db.relationship('Team', backref=backref('user', uselist=False), lazy=True)
-    created_sessions = db.relationship('Session', backref=backref('creator', uselist=False), lazy=True)
 
 
 class Team(db.Model):
@@ -55,10 +54,12 @@ class Session(db.Model):
     engine_pid = db.Column(db.Integer, nullable=True)
     created_on = db.Column(db.DateTime, server_default=db.func.now())
     updated_on = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
-    winner_id = db.Column('team_id', db.Integer, db.ForeignKey('team.id'))
-    teams = db.relationship('Team', secondary=team_session, backref='sessions')
+    winner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     created_by = db.Column('created_by', db.Integer, db.ForeignKey('user.id'))
 
+    creator = db.relationship('User', backref=backref('created_sessions', uselist=False), lazy=True, foreign_keys=[created_by])
+    winner = db.relationship('User', backref=backref('winned_sessions', uselist=False), lazy=True, foreign_keys=[winner_id])
+    teams = db.relationship('Team', secondary=team_session, backref='sessions')
 
 class Lobby(db.Model):
     id = db.Column(db.Integer, primary_key=True)
