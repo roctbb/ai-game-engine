d87d93ff1e5128a513552009c98d53a828d22812
Denis Indenbom <59681620+denisindenbom@users.noreply.github.com>
2024-01-11 20:01:13 +0300

    Implemented automatic addition of tic-tac-toe to the database

--- setup.py
+++ setup.py
@@ -8,4 +8,12 @@ system('flask db init')
 system('flask db migrate')
 system('flask db upgrade')
 
+# Add tic tac toe to database
+from server import app, Game, db
+
+with app.app_context():
+    game = Game(name='Tic Tac Toe', code='tic_tac_toe', team_number=2, team_size=1)
+    db.session.add(game)
+    db.session.commit()
+
 print('Run "python server.py" to start the engine!')
