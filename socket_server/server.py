5fa53bc6ec9f20e9e4daac925f7e32f646c4a665
denis <denis.indenbom@yandex.ru>
2025-02-20 14:42:56 +0300

    Code style cleaning (replace " with ')

--- socket_server/server.py
+++ socket_server/server.py
@@ -23,16 +23,16 @@ def subscribe_to_frames(data):
         session = get_session_by_id(session_id)
 
         if data.get('mode') == 'game':
-            if session.state == "ended":
-                socketio.emit("replay", json.dumps(session.replay), to=request.sid)
-                print("sent replay to", request.sid)
+            if session.state == 'ended':
+                socketio.emit('replay', json.dumps(session.replay), to=request.sid)
+                print('sent replay to', request.sid)
             else:
-                join_room(f"session_{session_id}")
-                socketio.emit("hello", "test", room=f"session_{session_id}")
-                print("joined to room", f"session_{session_id}")
+                join_room(f'session_{session_id}')
+                socketio.emit('hello', 'test', room=f'session_{session_id}')
+                print('joined to room', f'session_{session_id}')
 
         if data.get('mode') == 'stats':
-            socketio.emit("stats", json.dumps(session.stats), to=request.sid)
-            join_room(f"stats_{session_id}")
+            socketio.emit('stats', json.dumps(session.stats), to=request.sid)
+            join_room(f'stats_{session_id}')
     except NotFound:
-        socketio.emit("error", {"message": "Session not found"}, to=request.sid)
+        socketio.emit('error', {'message': 'Session not found'}, to=request.sid)
